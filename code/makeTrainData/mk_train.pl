cf6ea9a17c349a25332a21850c916b4f42769b85
SpilaLinks <146057259+SpilaLinks@users.noreply.github.com>
2023-09-27 16:56:14 +0900

    Add files via upload

---
+++ code/makeTrainData/mk_train.pl
@@ -0,0 +1,65 @@
+#!/usr/bin/perl
+#last appdated : 6/28
+
+use strict;
+use Encode;
+use utf8;
+
+use MeCab;
+my $model=new MeCab::Model();
+my $c=$model->createTagger();
+
+use File::Find;
+
+main();
+
+sub main(){
+
+  open(my $in, "senBERT.list");
+
+
+  while(my $line=decode_utf8(<$in>)){     #file単位のループ
+    chomp($line);
+
+    my @data=split(/ /, $line);
+    if($#data<=1){next;}
+
+    my $filename=$data[0];    #filename
+    my $label=$data[1];       #label
+
+    my %hash;
+    for(my $i=2; $i<=$#data; $i++){$hash{$data[$i]}++;}
+
+    open(my $in2, "TDNET/mk_txt/txt/$filename");
+    my $sid=1;
+
+
+    while(my $line=decode_utf8(<$in2>)){      #文単位のループ
+      chomp($line);
+
+      my @d1=split(/ /, $line);
+      $d1[1]=~s/\a//g;
+      $d1[1]=~s/\f//g;
+      $d1[1]=~s/\r//g;
+      $d1[1]=~s/\0//g;
+      $d1[1]=~s/\v//g;
+      $d1[1]=~s/\b//g;
+      $d1[1]=~s/\t//g;
+      $d1[1]=~s/　//g;
+      $d1[1]=~s/ //g;
+      if($d1[1] eq ""){next;}
+
+      my @d2=split(/:/, $d1[0]);  #原文書の文ID
+      my $gsid=$d2[1];
+
+      if(exists($hash{$gsid})){
+        print encode_utf8("$label $filename:$sid $d1[1]\n");
+        $sid++;
+      }
+
+    }#文単位
+    undef %hash;
+
+  }#file単位
+
+}
---
+++ code/makeTrainData/mk_train_2.pl
@@ -0,0 +1,55 @@
+#!/usr/bin/perl
+#last appdated : 6/14
+
+use strict;
+use Encode;
+use utf8;
+
+use MeCab;
+my $model=new MeCab::Model();
+my $c=$model->createTagger();
+
+use File::Find;
+
+main();
+
+sub main(){
+  open(my $in, "label.txt");
+  while(my $line=decode_utf8(<$in>)){  #file単位のループ
+
+    chomp($line);
+    my @data=split(/,/, $line);
+    if($#data<1){next;}
+
+    my @data2=split(/ /, $data[1]);
+    my $lb=$data2[0];
+    my $filename=$data[0];
+    open(my $in2, "TDNET/mk_txt/txt/$filename");
+    my $sid=1;
+
+
+
+    while(my $str=<$in2>){  #文単位のループ
+
+      my $ln=decode_utf8($str);
+      chomp($ln);
+      if($ln!~/[\p{Han}\p{Hiragana}\p{Katakana}]/){next;}
+      $ln=~s/\x0A//g;
+      $ln=~s/\x0D//g;
+      $ln=~s/\x0D?\x0A?$//;
+      $ln=~s/\a//;
+      $ln=~s/\f//;
+      $ln=~s/\r//;
+      $ln=~s/\0//;
+      $ln=~s/\v//;
+      $ln=~s/\b//;
+      $ln=~s/\t//;
+      if($ln eq ""){next;}
+
+      #sentenceBERT
+      my @dt=split(/ /, $ln);
+       print encode_utf8("$filename $lb $dt[1]\n");
+      if(`python3 calc_similarity.py $dt[1] $lb`==1){print encode_utf8("$lb $filename:$sid $dt[1]\n"); $sid++;}
+    }
+  }
+}
