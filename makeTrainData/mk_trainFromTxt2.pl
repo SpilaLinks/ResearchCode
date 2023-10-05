6c923f713152430750499793e06270eed358e9a7
Seikei1305 <us192046@cc.seikei.ac.jp>
2023-10-06 01:53:47 +0900

    train

--- makeTrainData/mk_trainFromTxt2.pl
+++ makeTrainData/mk_trainFromTxt2.pl
@@ -16,46 +16,42 @@ main();
 
 sub main(){
   print STDOUT "make ".OUTFILE." ...\n";
-  open(my $in, "../../data.list/sentenceBERT.list");
   open(my $out, ">../../data.list/".OUTFILE);
+  open(my $in, "../../data.list/label.list");
 
   while(my $line=decode_utf8(<$in>)){     #file単位のループ
     chomp($line);
 
-    my @data=split(/ /, $line);
-    if($#data<=1){next;}
+    my @filename_label=split(/,/, $line);
+    if($#filename_label<1){next;}
 
-    my $filename=$data[0];    #filename
-    my $label=$data[1];       #label
-
-    my %hash;
-    for(my $i=2; $i<=$#data; $i++){$hash{$data[$i]}++;}
+    my $filename=$filename_label[0];    #filename
+    my $label=$filename_label[1];       #label
 
     open(my $in2, "../../TDNET/mk_txt/txt2/$filename");
     my $sid=1;
-
-
     while(my $line=decode_utf8(<$in2>)){      #文単位のループ
       chomp($line);
 
-      my @d1=split(/ /, $line);
-      $d1[1]=~s/\a//g;
-      $d1[1]=~s/\f//g;
-      $d1[1]=~s/\r//g;
-      $d1[1]=~s/\0//g;
-      $d1[1]=~s/\v//g;
-      $d1[1]=~s/\b//g;
-      $d1[1]=~s/\t//g;
-      $d1[1]=~s/　//g;
-      $d1[1]=~s/ //g;
-      $d1[1]=replaceNamedEntity($d[1]);
-      if($d1[1] eq ""){next;}
+      my @id_sentence=split(/ /, $line);
+      $sentence=$id_sentence[1];
+      $sentence=~s/\a//g;
+      $sentence=~s/\f//g;
+      $sentence=~s/\r//g;
+      $sentence=~s/\0//g;
+      $sentence=~s/\v//g;
+      $sentence=~s/\b//g;
+      $sentence=~s/\t//g;
+      $sentence=~s/　//g;
+      $sentence=~s/ //g;
+      $sentence=replaceNamedEntity($sentencea);
+      if($sentence eq ""){next;}
 
-      my @d2=split(/:/, $d1[0]);  #原文書の文ID
+      my @d2=split(/:/, $id_sentence[0]);  #原文書の文ID
       my $gsid=$d2[1];
 
       if(exists($hash{$gsid})){
-        print $out encode_utf8("$label $filename:$sid $d1[1]\n");
+        print $out encode_utf8("$label $filename:$sid $sentence\n");
         $sid++;
       }
 
