5d3a11514065d35f984d1730d193cc75e8cde3bf
Seikei1305 <us192046@cc.seikei.ac.jp>
2023-10-17 05:22:34 +0900

    total_tfidf　途中

--- makeTrainData/mk_trainFromTxt2.pl
+++ makeTrainData/mk_trainFromTxt2.pl
@@ -11,6 +11,7 @@ my $c=$model->createTagger();
 use File::Find;
 
 use constant OUTFILE => "train.list";
+use constant THRESHOLD_TFIDF => 1;
 
 main();
 
@@ -21,43 +22,37 @@ sub main(){
 
   while(my $line=decode_utf8(<$in>)){     #file単位のループ
     chomp($line);
-
     my @filename_label=split(/,/, $line);
     if($#filename_label<1){next;}
 
     my $filename=$filename_label[0];    #filename
     my $label=$filename_label[1];       #label
 
+    my %tfidf=load_tfidf($filename);  #tfidf値の読み込み
+
     open(my $in2, "../../TDNET/mk_txt/txt2/$filename");
     my $sid=1;
-    while(my $line=decode_utf8(<$in2>)){      #文単位のループ
-      chomp($line);
+    while(my $str=<$in2>){      #文単位のループ
+      chomp($str);
+      my $string=deleteCntrL([split(/ /, $str)]->[1]);
+      my $line2=decode_utf8($str);
 
-      my @id_sentence=split(/ /, $line);
-      $sentence=$id_sentence[1];
-      $sentence=~s/\a//g;
-      $sentence=~s/\f//g;
-      $sentence=~s/\r//g;
-      $sentence=~s/\0//g;
-      $sentence=~s/\v//g;
-      $sentence=~s/\b//g;
-      $sentence=~s/\t//g;
+      my @id_sentence=split(/ /, $line2);
+      my $sentence=$id_sentence[1];
+      $sentence=~s/[[:cntrl:]]//g;
       $sentence=~s/　//g;
       $sentence=~s/ //g;
-      $sentence=replaceNamedEntity($sentencea);
+      #$sentence=replaceNamedEntity($sentence);
       if($sentence eq ""){next;}
 
-      my @d2=split(/:/, $id_sentence[0]);  #原文書の文ID
-      my $gsid=$d2[1];
+      #print ([split(/ /, $str)]->[1]."\n");
+      my $sum=calc_total_tfidf(%tfidf, $string);
+      #print encode_utf8("$sum : $sentence\n");
+      if(calc_total_tfidf(%tfidf, $sentence)<THRESHOLD_TFIDF){next;}
 
-      if(exists($hash{$gsid})){
-        print $out encode_utf8("$label $filename:$sid $sentence\n");
-        $sid++;
-      }
-
+      print $out encode_utf8("$label $filename:$sid $sentence\n");
+      $sid++;
     }#文単位
-    undef %hash;
-
   }#file単位
 
 }
@@ -73,4 +68,45 @@ sub replaceNamedEntity{
     $sentence=~s/$namedEntity/*/;
   }
   return $sentence;
+}
+
+sub load_tfidf{
+  my $filename=$_[0];
+  my %tfidf;
+
+  open(my $in, "../../data.list/tfidf.list");
+  my $flag=0;
+  while(my $line=decode_utf8(<$in>)){
+    if($flag==1 && $line!~"\t"){last;}
+    if($line=~$filename){$flag=1; next;}
+    if($flag){
+      my @word_tfidf=split(/: /, $line);
+      $tfidf{$word_tfidf[0]}=$word_tfidf[1];
+    }
+  }
+  close($in);
+  return %tfidf;
+}
+
+sub calc_total_tfidf{
+  my %tfidf=$_[0];
+  my $sentence=$_[1];
+  print ("$sentence\n");
+  my $sum_tfidf=0;
+  my $mecab_results=decode_utf8($c->parse($sentence));
+  my @split_result=split(/\n/, $mecab_results);
+  foreach my $word_info(@split_result){
+    if($word_info eq "EOS"){last;}
+    my $word=[split(/\t/, $word_info)]->[0];
+    #print encode_utf8("$word\n");
+    $sum_tfidf+=$tfidf{$word};
+  }
+  return $sum_tfidf;
+}
+
+sub deleteCntrL{
+  $_[0]=~s/[[:cntrl:]]//g;
+  $_[0]=~s/　//g;
+  $_[0]=~s/ //g;
+  return $_[0];
 }
