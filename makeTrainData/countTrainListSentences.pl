df896f28dc244152346986da79640dbe321ea308
Seikei1305 <us192046@cc.seikei.ac.jp>
2023-10-21 02:48:51 +0900

    minor change

--- makeTrainData/countTrainListSentences.pl
+++ makeTrainData/countTrainListSentences.pl
@@ -9,7 +9,7 @@ main();
 sub main{
   my @name=("上場会社の決定事実", "上場会社の発生事実", "上場会社の決算情報", "上場会社の業績予想、配当予想の修正等", "その他の情報", "子会社等の決定事実", "子会社等の発生事実", "子会社等の業績予想の修正等");
 
-  open(my $in, "../../data.list/label.list");
+  open(my $in, "<", "../../data.list/label.list") or die "can't open label.list:$!\n";
   my %hash;
   my $train=0;
   my $test=0;
@@ -23,7 +23,7 @@ sub main{
     $train++;
   }
 
-  print encode_utf8("     学習データのクラスごとの文数\n");
+  print encode_utf8("学習データのクラスごとの文数\n");
   for(my $i=0; $i<8; $i++){
     print encode_utf8("$i $name[$i] : $hash{$i}\n");
   }
