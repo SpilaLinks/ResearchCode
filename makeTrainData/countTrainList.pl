d193e22d344e7c458f06bea53798c52e7af51933
Seikei1305 <us192046@cc.seikei.ac.jp>
2023-10-27 02:38:46 +0900

    update

--- makeTrainData/countTrainList.pl
+++ makeTrainData/countTrainList.pl
@@ -7,7 +7,7 @@ use utf8;
 main();
 
 sub main{
-  my @name=("上場会社の決定事実", "上場会社の発生事実", "上場会社の決算情報", "上場会社の業績予想、配当予想の修正等", "その他の情報", "子会社等の決定事実", "子会社等の発生事実", "子会社等の業績予想の修正等");
+  my @name=("上場会社の決定事実", "上場会社の発生事実", "上場会社の決算情報", "上場会社の業績予想、配当予想の修正等", "その他の情報", "子会社等の決定事実", "子会社等の発生事実", "子会社等の業績予想の修正等", "該当なし");
 
   open(my $in, "<", "../../data.list/label.list") or die "can't open label.list:$!\n";
   my %hash;
@@ -24,7 +24,7 @@ sub main{
   }
 
   print encode_utf8("学習データのクラスごとの文数\n");
-  for(my $i=0; $i<8; $i++){
+  for(my $i=0; $i<9; $i++){
     print encode_utf8("$i $name[$i] : $hash{$i}\n");
   }
   print encode_utf8("学習データ文書数：$train\n");
